3698f77134809e66e79930655407b9b5c605fb20
polyshed-dev <erenmertkurum@gmail.com>
2026-06-09 21:30:43 +0300

    fix memory leak

--- Watermark/Config.cs
+++ Watermark/Config.cs
@@ -1,4 +1,7 @@
-﻿using Exiled.API.Interfaces;
+﻿using System.ComponentModel;
+using Exiled.API.Interfaces;
+
+// Copyright (c) 2026 polyshed
 
 namespace Watermark
 {
@@ -6,9 +9,9 @@ namespace Watermark
     {
         public bool IsEnabled { get; set; } = true;
         public bool Debug { get; set; } = false;
-
-        public string Text { get; set; } =
-            "<b><color=#F70505>Y</color><color=#EB0611>u</color><color=#DF071D>p</color><color=#D30829>o</color> <color=#BB0A41>D</color><color=#AF0B4D>i</color><color=#A30C59>y</color><color=#970D65>a</color><color=#8B0E71>r</color><color=#7F0F7D>ı</color> <color=#671195>》</color><color=#5B12A1>V</color><color=#4F13AD>a</color><color=#4314B9>n</color><color=#3715C5>i</color><color=#2B16D1>l</color><color=#1F17DD>l</color><color=#1318E9>a</color><color=#0719F5>+</color><color=#001AFF>+</color></b>";
+        [Description("The text to display as the watermark. Please use <color=#FFFFFF> even if you want the hint to be white.")]
+        public string Text { get; set; } = "your-server-name";
+        [Description("The vertical position of the watermark text.")]
         public float Position { get; set; }
     }
 }
--- Watermark/Plugin.cs
+++ Watermark/Plugin.cs
@@ -4,32 +4,40 @@ using Exiled.Events.EventArgs.Player;
 using RueI.API;
 using RueI.API.Elements;
 
+// Copyright (c) 2026 polyshed
+
 namespace Watermark
 {
   public class Plugin : Plugin<Config>
   {
-    public override string Name => "ServerWatermark";
-    public override string Author => "@polyshed.";
-    public override Version Version => new Version(1, 0, 0);
+    public static Plugin Instance {get; private set;}
+    
+    public override string Name { get; } = "ServerWatermark";
+    public override string Author  { get; } = "@polyshed.";
+    public override Version Version  { get; } = new Version(0, 1, 0);
+    public override Version RequiredExiledVersion { get; } = new Version(9, 14, 2);
 
-    private static readonly Tag tagWatermark = new Tag();
+    private static readonly Tag tagWatermark = new("watermarkElement");
+    
+    private static readonly BasicElement watermark = new BasicElement(Instance.Config.Position, Instance.Config.Text);
 
     public override void OnEnabled()
     {
       Exiled.Events.Handlers.Player.Verified += OnVerified;
+      Instance = this;
       base.OnEnabled();
     }
 
     public void OnVerified(VerifiedEventArgs ev)
     {
       RueDisplay watermarkDisplay = RueDisplay.Get(ev.Player);
-      BasicElement watermark = new BasicElement(Config.Position, Config.Text);
       watermarkDisplay.Show(tagWatermark,  watermark);
     }
 
     public override void OnDisabled()
     {
       Exiled.Events.Handlers.Player.Verified -= OnVerified;
+      Instance = null;
       base.OnDisabled();
     }
   }
